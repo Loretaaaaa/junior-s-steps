1b20eb7fa14b070448bf6fadaa693565528ce2e1
Loreta <badalyan.loreta@gmail.com>
2024-03-26 19:04:47 +0400

    some improvements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import Button from "./Button";
+import Steps from "./Steps";
 import StepMessage from "./StepMessage";
 
 const messages = [
@@ -9,48 +8,18 @@ const messages = [
 ];
 
 function App() {
-  const [step, setStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(true);
-
-  function handlePrevious() {
-    if (step > 1) setStep((prevStep) => prevStep - 1);
-  }
-
-  function handleNext() {
-    if (step < 3) setStep((prevStep) => prevStep + 1);
-  }
-
   return (
-    <>
-      <button
-        className="close"
-        onClick={() => setIsOpen((prevState) => !prevState)}
-      >
-        X
-      </button>
-      {isOpen && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
-          </div>
-          <StepMessage step={step}>
-          {messages[step - 1]}
-          </StepMessage>
-          <div className="buttons">
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
-              <span>👈</span> Previous
-            </Button>
-
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
-              Next <span>👉</span>
-              <span>🤓</span>
-            </Button>
-          </div>
-        </div>
-      )}
-    </>
+    <div>
+      <Steps messages={messages} />
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>✌️</p>
+      </StepMessage>
+      <StepMessage step={2}>
+        <p>Read children prop</p>
+        <p>😎</p>
+      </StepMessage>
+    </div>
   );
 }
 
